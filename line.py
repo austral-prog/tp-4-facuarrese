2187cf9f3641b4d0aeaa029528b7257298e05077
facuarrese <162734700+facuarrese@users.noreply.github.com>
2024-04-12 11:29:11 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,2 +1,10 @@
 def line():
     print("TO DO")
+a=float(imput("ingrese el coeficiente A: "))
+b=float(imput("ingrese el coeficiente B: "))
+X1=float(imput("ingrese el coeficiente X1: "))
+X2=float(imput("ingrese el coeficiente X2: "))
+Y1=(AX1+B)
+Y2=(AX2+B)
+distancia=(sqrt((X2-X1)2+(Y2-Y1)2))
+print("la distancia es: ", distancia)
